96eacfa8ba33166a802f91fcb577f4fb89cd2b4c
pomponchik <zheni-b@yandex.ru>
2019-02-19 04:13:07 +0300

    add reads function

---
+++ head.h
@@ -0,0 +1,7 @@
+typedef struct			s_tetr
+{
+	char **str;
+	unsigned int height;
+  unsigned int width;
+	struct s_tetr		*next;
+}						t_etra;
--- reading.c
+++ reading.c
@@ -12,6 +12,74 @@ int letter_prove(char *str)
   return (1);
 }
 
+void ft_lst_free_chain(t_list *chain)
+{
+  t_list *temp;
+
+  while (chain)
+  {
+    temp = chain->next;
+    free(chain->content);
+    free(chain);
+    chain = temp;
+  }
+}
+
+t_list *lst_reverse_helper(t_list *begin, t_list *end)
+{
+  end->next = begin;
+  while (begin)
+  {
+    if (begin->next == end)
+      break ;
+    begin = begin->next;
+  }
+  begin->next = NULL;
+  return (end);
+}
+
+t_list *ft_lst_reverse(t_list *begin)
+{
+  t_list *temp_begin;
+  t_list *end;
+
+  temp_begin = begin;
+  while (end != temp_begin)
+  {
+    end = begin;
+    while (end->next)
+    {
+      end = end->next;
+    }
+    begin = lst_reverse_helper(begin, end);
+  }
+}
+
+t_etra *reader(int fd)
+{
+  char *line;
+  int r;
+  int count;
+  t_list *temp;
+  t_list *temp_res;
+
+  count = 0;
+  line = NULL;
+  while ((r = get_next_line(fd, char &line)))
+  {
+    if (line && !ft_strlen(line))
+    {
+
+      count = 0;
+    }
+    else if (line && ft_strlen(line))
+    {
+      temp = ft_lstnew(line, ft_strlen(line));
+      temp_res = ft_lstadd(lst, temp);
+    }
+  }
+}
+
 //ft_lst_iter(lst)
 
 int main()
