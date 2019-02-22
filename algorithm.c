d13352dee0f5adfc7196a2ba78cf54f942eed769
pomponchik <zheni-b@yandex.ru>
2019-02-23 02:46:01 +0300

    added some lines to algorithm

--- algorithm.c
+++ algorithm.c
@@ -12,29 +12,66 @@
 
 #include "head.h"
 
-int size_prove(size_t x, size_t y, size_t size)
+typedef struct			s_puter
 {
-	
+	size_t x;
+	size_t y;
+	size_t size;
+}						t_put;
+
+int size_prove(t_put squad, t_etra *tet)
+{
+	if (squad.size < squad.x + tet->width + 1)
+		return (0);
+	if (squad.size < squad.y + tet->height + 1)
+		return (0);
+	return (1);
 }
 
-int try_to_put(char **map, size_t x, size_t y, size_t size)
-{
 
+
+int try_to_put(char **map, t_etra	*tets, t_put put)
+{
+	char **figure;
+	char *line;
+	//Сначала место под фигуру и проверить, что все нужные клетки свободны
+	//если все ок, ставим фигуру и возвращаем 0
+	figure = tets->str;
+	while(*figure)
+	{
+		line = *figure;
+		while (*line)
+		{
+			if ()
+			line++;
+		}
+		put.y++;
+		figure++;
+	}
 }
 
 int algorithm(char **map, t_etra	*tets, size_t size)
 {
-	size_t x;
-	size_t y;
+	t_put i;
 
-	x = 0;
-	y = 0;
+	i.y = 0;
+	i.size = size;
 
 	if (!tets)
 		return (1);
 	while (y < (size - 1))
 	{
-		try_to_put;
+		i.x = 0;
+		while (i.x + 1 < i.size)
+		{
+			if (size_prove(i, tets) && try_to_put)
+			{
+				;
+				if (algorithm(map, tets->next, size))
+					return (1);
+			}
+			i.x++;
+		}
 		y++;
 	}
 }
